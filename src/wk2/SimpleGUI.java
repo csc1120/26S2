b1c908e6a033c4254d0476c1ce7a9b39c793d8ef
Dr. Taylor <taylor@msoe.edu>
2026-01-27 15:17:46 -0600

    Week 2 Lecture 2

--- src/wk2/SimpleGUI.java
+++ src/wk2/SimpleGUI.java
@@ -1,15 +1,20 @@
 package wk2;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.BoxBlur;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class SimpleGUI extends Application {
     private static final int WIDTH = 400;
-    private static final int HEIGHT = 400;
+    private static final int HEIGHT = 300;
+    private Label label;
 
     public static void main(String[] args) {
         launch(args);
@@ -18,12 +23,24 @@ public class SimpleGUI extends Application {
     @Override
     public void start(Stage primaryStage) {
         Pane root = new StackPane();
+        label = new Label("Here is some text that can be manipulated with the button above.");
         Button clickMe = new Button("Click Me");
         clickMe.setOnAction(new ClickMeHandler());
-        root.getChildren().add(clickMe);
+        root.getChildren().addAll(label, clickMe);
 
         primaryStage.setTitle("Simple GUI");
         primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
         primaryStage.show();
+    }
+
+    private class ClickMeHandler implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent event) {
+            if(label.getEffect()==null) {
+                label.setEffect(new BoxBlur());
+            } else {
+                label.setEffect(null);
+            }
+        }
     }
 }
